bb688cf8dbd887f30641f3d0ecfaca62d9c114d4
MZildhanReynaldi <zildhanreynaldi99@gmail.com>
2020-11-15 23:26:25 +0800

    memnambahkan beberapa abstrak class yang terimplemantasi terhadap interface item dan packing

---
+++ src/minimarket/Kebab.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minimarket;
+public abstract class Kebab implements Item {
+
+   public Packing packing() {
+      return new Pembungkus ();
+   }
+
+   public abstract float harga();
+}
---
+++ src/minimarket/Pembungkus.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minimarket;
+
+public class Pembungkus implements Packing {
+
+   @Override
+   public String pack() {
+      return "Pembungkus";
+   }
+}
