b497acf48eebeb281034878f334c481c0dd63349
MDimasPrimaA <Dimasprima1212@gmail.com>
2020-11-16 11:32:51 +0800

    menambahkan main class tes 2

--- src/minimarket/Minimarket.java
+++ src/minimarket/Minimarket.java
@@ -39,6 +39,40 @@ public class Minimarket {
         TokoBaris.getBaris("Minuman").isibaris();
         TokoBaris.getBaris("Minuman").isibaris();
         TokoBaris.getBaris("Makanan").isibaris();
+        
+        
+         System.out.println("\n\nComposite Pattern ");
+         Karyawan BOS = new Karyawan("Dimas","Bos",100000000);
+        
+        Karyawan headPenjualan = new Karyawan("Zildhan", "Kepala Penjualan",5000000);
+        
+        Karyawan headPemasaran = new Karyawan("Galih","Kepala Pemasaran",5000000);
+        
+        Karyawan Pemasaran1 = new Karyawan("Andri","Pemasaran",1000000);
+        Karyawan Pemasaran2 = new Karyawan("Lina","Pemasaran",1000000);
+        
+        Karyawan Penjualan1 = new Karyawan("Mery","Penjualan",1000000);
+        Karyawan Penjualan2 = new Karyawan("Rizky","Penjualan",1000000);
+        BOS.add(headPenjualan);
+        BOS.add(headPemasaran);
+        
+        headPemasaran.add(Pemasaran1);
+        headPemasaran.add(Pemasaran2);
+        
+        headPenjualan.add(Penjualan1);
+        headPenjualan.add(Penjualan2);
+        
+        System.out.println(BOS);
+        
+        for(Karyawan headKaryawan : BOS.getSubordinates()){
+            System.out.println();
+            System.out.println(headKaryawan);
+     
+            for(Karyawan Karyawan : headKaryawan.getSubordinates()){
+                System.out.println(" - "+Karyawan);
+            
     }
     
+}
+    }
 }
