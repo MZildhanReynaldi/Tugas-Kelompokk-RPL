d943f88f6b5af0abafbf71b48f72e5aae9746986
rizkirosga83 <rizki.rosga83@gmail.com>
2020-11-16 11:44:56 +0800

    menambahkan interface observer

---
+++ src/minimarket/Observer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minimarket;
+
+/**
+ *
+ * @author Rizki Aprido Rosga
+ */
+public interface Observer {
+    public void update(double tehpucukPrice, double nescafePrice, double onigiriPrice);
+}
