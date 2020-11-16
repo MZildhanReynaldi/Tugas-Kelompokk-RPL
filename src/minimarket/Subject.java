e42fba9f0b988f1e2ec6604e8c872ecffe9da4dc
rizkirosga83 <rizki.rosga83@gmail.com>
2020-11-16 11:42:14 +0800

    menambahkan class subject pada method observer

---
+++ src/minimarket/Subject.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minimarket;
+
+/**
+ *
+ * @author Rizki Aprido Rosga
+ */
+public interface Subject {
+    public void register(Observer o);
+    public void unregister(Observer o);
+    public void notifyObserver();
+}
